24d01866afa74e751abe66144ae9f944b271ae4e
jmbhughes <hughes.jmb@gmail.com>
2022-12-12 15:14:05 -0700

    init

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='simpunch',
+    version='0.0.1',
+    packages=['simpunch'],
+    url='',
+    license='MIT',
+    author='J. Marcus Hughes',
+    author_email='marcus.hughes@swri.org',
+    description='simulator for PUNCH-like data'
+)
