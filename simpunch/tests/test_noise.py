5d062fa57dadb6c50380cee46d21d674e5484193
jmbhughes <hughes.jmb@gmail.com>
2022-12-13 11:44:33 -0700

    removed encapsulating classes

--- simpunch/tests/test_noise.py
+++ simpunch/tests/test_noise.py
@@ -1,11 +1,10 @@
 import numpy as np
-import pytest
-from simpunch.noise import Noise
+from simpunch.noise import generate_noise
 
 
 def test_noise_generation():
     arr_dim = 2048
     arr = np.random.random([arr_dim, arr_dim]) * (2**16)
-    noise_arr = Noise.gen_noise(arr)
+    noise_arr = generate_noise(arr)
 
     assert noise_arr.shape == arr.shape
