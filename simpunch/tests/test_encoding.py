7e22aa497d2d876c8d4faba52d732040aabad46d
jmbhughes <hughes.jmb@gmail.com>
2022-12-14 09:35:50 -0700

    added simple circular encode/decode test

--- simpunch/tests/test_encoding.py
+++ simpunch/tests/test_encoding.py
@@ -9,7 +9,7 @@ def test_encoding():
     encoded_arr = encode(arr, frombits=16, tobits=10)
 
     assert encoded_arr.shape == arr.shape
-    assert encoded_arr.max() <= 2**10
+    assert np.max(encoded_arr) <= 2**10
 
 
 def test_decoding():
@@ -19,4 +19,14 @@ def test_decoding():
     decoded_arr = decode(arr, frombits=10, tobits=16)
 
     assert decoded_arr.shape == arr.shape
-    assert decoded_arr.max() <= 2**16
+    assert np.max(decoded_arr) <= 2**16
+
+
+def test_encode_decode_circular():
+    arr_dim = 2048
+    original_arr = (np.random.random([arr_dim, arr_dim]) * (2**16)).astype(int)
+
+    encoded_arr = encode(original_arr, frombits=16, tobits=16)
+    decoded_arr = decode(encoded_arr, frombits=16, tobits=16)
+
+    assert np.allclose(original_arr, decoded_arr)
