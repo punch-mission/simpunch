727702809143226fccc9bc09a6686348c8837364
Marcus Hughes <hughes.jmb@gmail.com>
2024-03-26 08:46:46 -0600

    adds wcs improvements to level 3 generation

--- simpunch/generate-level3.py
+++ simpunch/generate-level3.py
@@ -12,11 +12,21 @@ import glob
 
 from astropy.wcs import WCS
 from astropy.io import fits
+from astropy.wcs.utils import add_stokes_axis_to_wcs
+from astropy.time import Time
+from astropy.coordinates import get_sun
 import reproject
+import scipy.ndimage
+
 
 from datetime import datetime, timedelta
 
 from punchbowl.data import PUNCHData, NormalizedMetadata
+
+
+def get_sun_ra_dec(dt: datetime):
+    position =  get_sun(Time(str(dt), scale='utc'))
+    return position.ra.value, position.dec.value
 
 
 def define_mask(shape=(4096, 4096), distance_value=0.68):
@@ -44,13 +54,17 @@ def assemble_punchdata(input_tb, input_pb, wcs, product_code, product_level, mas
     """Assemble a punchdata object with correct metadata"""
 
     with fits.open(input_tb) as hdul:
-        data_tb = hdul[0].data
+        data_tb = hdul[1].data
+        data_tb = scipy.ndimage.zoom(data_tb, 2, order=0)
         data_tb[np.where(data_tb == -9999.0)] = 0
-        if mask is not None: data_tb = data_tb * mask
+        if mask is not None:
+            data_tb = data_tb * mask
     with fits.open(input_pb) as hdul:
-        data_pb = hdul[0].data
+        data_pb = hdul[1].data
+        data_pb = scipy.ndimage.zoom(data_pb, 2, order=0)
         data_pb[np.where(data_pb == -9999.0)] = 0
-        if mask is not None: data_pb = data_pb * mask
+        if mask is not None:
+            data_pb = data_pb * mask
 
     datacube = np.stack([data_tb, data_pb]).astype('float32')
 
@@ -71,9 +85,10 @@ def generate_l3_ptm(input_tb, input_pb, path_output, time_obs, time_delta, rotat
     mosaic_shape = (4096, 4096)
     mosaic_wcs = WCS(naxis=2)
     mosaic_wcs.wcs.crpix = mosaic_shape[1] / 2 - 0.5, mosaic_shape[0] / 2 - 0.5
-    mosaic_wcs.wcs.crval = 0, 0
+    mosaic_wcs.wcs.crval = get_sun_ra_dec(time_obs + time_delta)
     mosaic_wcs.wcs.cdelt = 0.0225, 0.0225
-    mosaic_wcs.wcs.ctype = "HPLN-ARC", "HPLT-ARC"
+    mosaic_wcs.wcs.ctype = "RA---ARC", "DEC--ARC"
+    mosaic_wcs = add_stokes_axis_to_wcs(mosaic_wcs, 0)
 
     # Mask data to define the field of view
     mask = define_trefoil_mask(rotation_stage=rotation_stage)
@@ -82,15 +97,15 @@ def generate_l3_ptm(input_tb, input_pb, path_output, time_obs, time_delta, rotat
     pdata = assemble_punchdata(input_tb, input_pb, mosaic_wcs, product_code='PTM', product_level='3', mask=mask)
 
     # Update required metadata
-    tstring_start = time_obs.strftime('%Y-%m-%dT%H:%M:%S.00')
-    tstring_end = (time_obs + time_delta).strftime('%Y-%m-%dT%H:%M:%S.00')
-    tstring_avg = (time_obs + time_delta / 2).strftime('%Y-%m-%dT%H:%M:%S.00')
+    tstring_start = time_obs.strftime('%Y-%m-%dT%H:%M:%S.000')
+    tstring_end = (time_obs + time_delta).strftime('%Y-%m-%dT%H:%M:%S.000')
+    tstring_avg = (time_obs + time_delta / 2).strftime('%Y-%m-%dT%H:%M:%S.000')
 
-    pdata.meta['DATE-OBS'].value = tstring_start
-    pdata.meta['DATE-BEG'].value = tstring_start
-    pdata.meta['DATE-END'].value = tstring_end
-    pdata.meta['DATE-AVG'].value = tstring_avg
-    pdata.meta['DATE'].value = (time_obs + time_delta + timedelta(hours=12)).strftime('%Y-%m-%dT%H:%M:%S.00')
+    pdata.meta['DATE-OBS'] = tstring_start
+    pdata.meta['DATE-BEG'] = tstring_start
+    pdata.meta['DATE-END'] = tstring_end
+    pdata.meta['DATE-AVG'] = tstring_avg
+    pdata.meta['DATE'] = (time_obs + time_delta + timedelta(hours=12)).strftime('%Y-%m-%dT%H:%M:%S.000')
 
     # Write out
     pdata.write(path_output + pdata.filename_base + '.fits')
@@ -103,17 +118,17 @@ def generate_l3_pnn(input_tb, input_pb, path_output, time_obs, time_delta):
     mosaic_shape = (4096, 4096)
     mosaic_wcs = WCS(naxis=2)
     mosaic_wcs.wcs.crpix = mosaic_shape[1] / 2 - 0.5, mosaic_shape[0] / 2 - 0.5
-    mosaic_wcs.wcs.crval = 0, 0
+    mosaic_wcs.wcs.crval = get_sun_ra_dec(time_obs + time_delta)
     mosaic_wcs.wcs.cdelt = 0.0225, 0.0225
-    mosaic_wcs.wcs.ctype = "HPLN-ARC", "HPLT-ARC"
+    mosaic_wcs.wcs.ctype = "RA---ARC", "DEC--ARC"
 
     # Define the NFI WCS
     nfi1_shape = [2048, 2048]
     nfi1_wcs = WCS(naxis=2)
     nfi1_wcs.wcs.crpix = nfi1_shape[1] / 2 - 0.5, nfi1_shape[0] / 2 - 0.5
-    nfi1_wcs.wcs.crval = 0, 0
+    nfi1_wcs.wcs.crval = get_sun_ra_dec(time_obs + time_delta)
     nfi1_wcs.wcs.cdelt = 0.01, 0.01
-    nfi1_wcs.wcs.ctype = "HPLN-TAN", "HPLT-TAN"
+    nfi1_wcs.wcs.ctype = "RA---TAN", "DEC--TAN"
 
     # Mask data to define the field of view
     mask = define_mask(shape=(4096, 4096), distance_value=0.155)
@@ -132,18 +147,26 @@ def generate_l3_pnn(input_tb, input_pb, path_output, time_obs, time_delta):
     uncert = (np.sqrt(reprojected_data) / np.sqrt(reprojected_data).max() * 255).astype('uint8')
 
     meta = NormalizedMetadata.load_template('PAN', '3')
+    tstring_start = time_obs.strftime('%Y-%m-%dT%H:%M:%S.000')
+    tstring_end = (time_obs + time_delta).strftime('%Y-%m-%dT%H:%M:%S.000')
+    tstring_avg = (time_obs + time_delta / 2).strftime('%Y-%m-%dT%H:%M:%S.000')
+    meta['DATE-OBS'] = tstring_start
+    meta['DATE-BEG'] = tstring_start
+    meta['DATE-END'] = tstring_end
+    meta['DATE-AVG'] = tstring_avg
+    nfi1_wcs = add_stokes_axis_to_wcs(nfi1_wcs, 0)
     outdata = PUNCHData(data=reprojected_data, wcs=nfi1_wcs, meta=meta, uncertainty=uncert)
 
     # Update required metadata
-    tstring_start = time_obs.strftime('%Y-%m-%dT%H:%M:%S.00')
-    tstring_end = (time_obs + time_delta).strftime('%Y-%m-%dT%H:%M:%S.00')
-    tstring_avg = (time_obs + time_delta / 2).strftime('%Y-%m-%dT%H:%M:%S.00')
+    tstring_start = time_obs.strftime('%Y-%m-%dT%H:%M:%S.000')
+    tstring_end = (time_obs + time_delta).strftime('%Y-%m-%dT%H:%M:%S.000')
+    tstring_avg = (time_obs + time_delta / 2).strftime('%Y-%m-%dT%H:%M:%S.000')
 
-    pdata.meta['DATE-OBS'].value = tstring_start
-    pdata.meta['DATE-BEG'].value = tstring_start
-    pdata.meta['DATE-END'].value = tstring_end
-    pdata.meta['DATE-AVG'].value = tstring_avg
-    pdata.meta['DATE'].value = (time_obs + time_delta + timedelta(hours=12)).strftime('%Y-%m-%dT%H:%M:%S.00')
+    pdata.meta['DATE-OBS'] = tstring_start
+    pdata.meta['DATE-BEG'] = tstring_start
+    pdata.meta['DATE-END'] = tstring_end
+    pdata.meta['DATE-AVG'] = tstring_avg
+    pdata.meta['DATE'] = (time_obs + time_delta + timedelta(hours=12)).strftime('%Y-%m-%dT%H:%M:%S.000')
 
     # Write out
     outdata.write(path_output + pdata.filename_base + '.fits')
@@ -156,9 +179,11 @@ def generate_l3_pam(input_tb, input_pb, path_output, time_obs, time_delta):
     mosaic_shape = (4096, 4096)
     mosaic_wcs = WCS(naxis=2)
     mosaic_wcs.wcs.crpix = mosaic_shape[1] / 2 - 0.5, mosaic_shape[0] / 2 - 0.5
-    mosaic_wcs.wcs.crval = 0, 0
+    mosaic_wcs.wcs.crval = get_sun_ra_dec(time_obs + time_delta)
     mosaic_wcs.wcs.cdelt = 0.0225, 0.0225
-    mosaic_wcs.wcs.ctype = "HPLN-ARC", "HPLT-ARC"
+    mosaic_wcs.wcs.ctype = "RA---ARC", "DEC--ARC"
+    mosaic_wcs = add_stokes_axis_to_wcs(mosaic_wcs, 0)
+
 
     # Mask data to define the field of view
     mask = define_mask(shape=(4096, 4096), distance_value=0.68)
@@ -167,15 +192,15 @@ def generate_l3_pam(input_tb, input_pb, path_output, time_obs, time_delta):
     pdata = assemble_punchdata(input_tb, input_pb, mosaic_wcs, product_code='PAM', product_level='3', mask=mask)
 
     # Update required metadata
-    tstring_start = time_obs.strftime('%Y-%m-%dT%H:%M:%S.00')
-    tstring_end = (time_obs + time_delta).strftime('%Y-%m-%dT%H:%M:%S.00')
-    tstring_avg = (time_obs + time_delta / 2).strftime('%Y-%m-%dT%H:%M:%S.00')
+    tstring_start = time_obs.strftime('%Y-%m-%dT%H:%M:%S.000')
+    tstring_end = (time_obs + time_delta).strftime('%Y-%m-%dT%H:%M:%S.000')
+    tstring_avg = (time_obs + time_delta / 2).strftime('%Y-%m-%dT%H:%M:%S.000')
 
-    pdata.meta['DATE-OBS'].value = tstring_start
-    pdata.meta['DATE-BEG'].value = tstring_start
-    pdata.meta['DATE-END'].value = tstring_end
-    pdata.meta['DATE-AVG'].value = tstring_avg
-    pdata.meta['DATE'].value = (time_obs + time_delta + timedelta(hours=12)).strftime('%Y-%m-%dT%H:%M:%S.00')
+    pdata.meta['DATE-OBS'] = tstring_start
+    pdata.meta['DATE-BEG'] = tstring_start
+    pdata.meta['DATE-END'] = tstring_end
+    pdata.meta['DATE-AVG'] = tstring_avg
+    pdata.meta['DATE'] = (time_obs + time_delta + timedelta(hours=12)).strftime('%Y-%m-%dT%H:%M:%S.000')
 
     # Write out
     pdata.write(path_output + pdata.filename_base + '.fits')
@@ -188,17 +213,17 @@ def generate_l3_pan(input_tb, input_pb, path_output, time_obs, time_delta):
     mosaic_shape = (4096, 4096)
     mosaic_wcs = WCS(naxis=2)
     mosaic_wcs.wcs.crpix = mosaic_shape[1] / 2 - 0.5, mosaic_shape[0] / 2 - 0.5
-    mosaic_wcs.wcs.crval = 0, 0
+    mosaic_wcs.wcs.crval = get_sun_ra_dec(time_obs + time_delta)
     mosaic_wcs.wcs.cdelt = 0.0225, 0.0225
-    mosaic_wcs.wcs.ctype = "HPLN-ARC", "HPLT-ARC"
+    mosaic_wcs.wcs.ctype = "RA---ARC", "DEC--ARC"
 
     # Define the NFI WCS
     nfi1_shape = [2048, 2048]
     nfi1_wcs = WCS(naxis=2)
     nfi1_wcs.wcs.crpix = nfi1_shape[1] / 2 - 0.5, nfi1_shape[0] / 2 - 0.5
-    nfi1_wcs.wcs.crval = 0, 0
+    nfi1_wcs.wcs.crval = get_sun_ra_dec(time_obs + time_delta)
     nfi1_wcs.wcs.cdelt = 0.01, 0.01
-    nfi1_wcs.wcs.ctype = "HPLN-TAN", "HPLT-TAN"
+    nfi1_wcs.wcs.ctype = "RA---TAN", "DEC--TAN"
 
     # Mask data to define the field of view
     mask = define_mask(shape=(4096, 4096), distance_value=0.155)
@@ -217,34 +242,45 @@ def generate_l3_pan(input_tb, input_pb, path_output, time_obs, time_delta):
     uncert = (np.sqrt(reprojected_data) / np.sqrt(reprojected_data).max() * 255).astype('uint8')
 
     meta = NormalizedMetadata.load_template('PAN', '3')
+    tstring_start = time_obs.strftime('%Y-%m-%dT%H:%M:%S.000')
+    tstring_end = (time_obs + time_delta).strftime('%Y-%m-%dT%H:%M:%S.000')
+    tstring_avg = (time_obs + time_delta / 2).strftime('%Y-%m-%dT%H:%M:%S.000')
+    meta['DATE-OBS'] = tstring_start
+    meta['DATE-BEG'] = tstring_start
+    meta['DATE-END'] = tstring_end
+    meta['DATE-AVG'] = tstring_avg
+    meta['DATE'] = (time_obs + time_delta + timedelta(hours=12)).strftime('%Y-%m-%dT%H:%M:%S.000')
+    nfi1_wcs = add_stokes_axis_to_wcs(nfi1_wcs, 0)
     outdata = PUNCHData(data=reprojected_data, wcs=nfi1_wcs, meta=meta, uncertainty=uncert)
 
     # Update required metadata
-    tstring_start = time_obs.strftime('%Y-%m-%dT%H:%M:%S.00')
-    tstring_end = (time_obs + time_delta).strftime('%Y-%m-%dT%H:%M:%S.00')
-    tstring_avg = (time_obs + time_delta / 2).strftime('%Y-%m-%dT%H:%M:%S.00')
+    tstring_start = time_obs.strftime('%Y-%m-%dT%H:%M:%S.000')
+    tstring_end = (time_obs + time_delta).strftime('%Y-%m-%dT%H:%M:%S.000')
+    tstring_avg = (time_obs + time_delta / 2).strftime('%Y-%m-%dT%H:%M:%S.000')
 
-    pdata.meta['DATE-OBS'].value = tstring_start
-    pdata.meta['DATE-BEG'].value = tstring_start
-    pdata.meta['DATE-END'].value = tstring_end
-    pdata.meta['DATE-AVG'].value = tstring_avg
-    pdata.meta['DATE'].value = (time_obs + time_delta + timedelta(hours=12)).strftime('%Y-%m-%dT%H:%M:%S.00')
+    pdata.meta['DATE-OBS'] = tstring_start
+    pdata.meta['DATE-BEG'] = tstring_start
+    pdata.meta['DATE-END'] = tstring_end
+    pdata.meta['DATE-AVG'] = tstring_avg
+    pdata.meta['DATE'] = (time_obs + time_delta + timedelta(hours=12)).strftime('%Y-%m-%dT%H:%M:%S.000')
 
     # Write out
     outdata.write(path_output + pdata.filename_base + '.fits')
 
 
-def generate_l3_all(datadir='/Users/clowder/data/punch/'):
+def generate_l3_all(datadir='/Users/jhughes/Desktop/data/GAMERA Mosaic data TB and PB jan 2024 /'):
     """Generate all level 3 synthetic data"""
 
     # Set file output path
     outdir = datadir + 'synthetic_L3/'
 
     # Parse list of model data
-    files_tb = glob.glob(datadir + 'synthetic_cme/TB*.fits')
-    files_pb = glob.glob(datadir + 'synthetic_cme/PB*.fits')
+    print(datadir + 'synthetic_cme/*_TB*.fits')
+    files_tb = glob.glob(datadir + 'synthetic_cme/*_TB*.fits')
+    files_pb = glob.glob(datadir + 'synthetic_cme/*_PB*.fits')
     files_tb.sort()
     files_pb.sort()
+    print(len(files_tb), len(files_pb))
 
     # Stack and repeat these data for testing - about 25 times to get around 5 days of data
     files_tb = np.tile(files_tb, 25)
@@ -264,12 +300,12 @@ def generate_l3_all(datadir='/Users/clowder/data/punch/'):
     # Run individual generators
     for i, (file_tb, file_pb, time_obs) in enumerate(zip(files_tb, files_pb, times_obs)):
         rotation_stage = i % 8
-        # print(str(rotation_stage) + ' - PTM / PNN - ' + time_obs.strftime('%Y-%m-%dT%H:%M:%S.00'))
+        # print(str(rotation_stage) + ' - PTM / PNN - ' + time_obs.strftime('%Y-%m-%dT%H:%M:%S.000'))
         generate_l3_ptm(file_tb, file_pb, outdir, time_obs, time_delta, rotation_stage)
         generate_l3_pnn(file_tb, file_pb, outdir, time_obs, time_delta)
 
         if rotation_stage == 0:
-            # print(str(rotation_stage) + ' - PAM / PAN - ' + time_obs.strftime('%Y-%m-%dT%H:%M:%S.00'))
+            # print(str(rotation_stage) + ' - PAM / PAN - ' + time_obs.strftime('%Y-%m-%dT%H:%M:%S.000'))
             generate_l3_pam(file_tb, file_pb, outdir, time_obs, time_delta_ln)
             generate_l3_pan(file_tb, file_pb, outdir, time_obs, time_delta_ln)
 
