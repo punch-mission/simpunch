1708af53a7e92f617951e78eea424b7eefa3aaa1
Marcus Hughes <hughes.jmb@gmail.com>
2024-11-12 12:50:59 -0700

    fix quartic writing

--- create_quartic_fit_coeffs.py
+++ create_quartic_fit_coeffs.py
@@ -1,5 +1,11 @@
 # ruff: noqa
+from datetime import datetime
+
 from astropy.io.fits import CompImageHDU, HDUList, ImageHDU, PrimaryHDU
+from astropy.wcs import WCS
+from astropy.wcs.docstrings import naxis
+from ndcube import NDCube
+from punchbowl.data import NormalizedMetadata, write_ndcube_to_fits
 from punchbowl.data.io import load_ndcube_from_fits
 from punchbowl.level1.quartic_fit import create_constant_quartic_coefficients
 
@@ -9,11 +15,17 @@ nfi_vignetting_model_path = "./build_3_review_files/PUNCH_L1_GM4_20240819045110_
 wfi_vignette = load_ndcube_from_fits(wfi_vignetting_model_path).data[...] + 1E-8
 nfi_vignette = load_ndcube_from_fits(nfi_vignetting_model_path).data[...] + 1E-8
 
-wfi_quartic = create_constant_quartic_coefficients((2048, 2048)).T
-nfi_quartic = create_constant_quartic_coefficients((2048, 2048)).T
+wfi_quartic = create_constant_quartic_coefficients((2048, 2048))
+nfi_quartic = create_constant_quartic_coefficients((2048, 2048))
 
 wfi_quartic[-2, :, :] /= wfi_vignette
 nfi_quartic[-2, :, :] /= nfi_vignette
 
-HDUList([PrimaryHDU(), CompImageHDU(data=wfi_quartic)]).writeto("wfi_quartic_coeffs.fits")
-HDUList([PrimaryHDU(), CompImageHDU(data=nfi_quartic)]).writeto("nfi_quartic_coeffs.fits")
+meta = NormalizedMetadata.load_template("FQ1", "0")
+meta['DATE-OBS'] = str(datetime.now())
+
+wfi_cube = NDCube(data=wfi_quartic, meta=meta, wcs=WCS(naxis=3))
+nfi_cube = NDCube(data=nfi_quartic, meta=meta, wcs=WCS(naxis=3))
+
+write_ndcube_to_fits(wfi_cube.data, "wfi_quartic_coeffs.fits")
+write_ndcube_to_fits(nfi_cube.data, "nfi_quartic_coeffs.fits")
