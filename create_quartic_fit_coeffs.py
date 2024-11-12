11a13a5faeaf75ff6841b60f13113bbc9f58253d
Marcus Hughes <hughes.jmb@gmail.com>
2024-11-12 12:11:08 -0700

    prevent zeros in vignetting

--- create_quartic_fit_coeffs.py
+++ create_quartic_fit_coeffs.py
@@ -10,14 +10,14 @@ from punchbowl.level1.quartic_fit import create_constant_quartic_coefficients
 wfi_vignetting_model_path = "./build_3_review_files/PUNCH_L1_GM1_20240817174727_v2.fits"
 nfi_vignetting_model_path = "./build_3_review_files/PUNCH_L1_GM4_20240819045110_v1.fits"
 
-wfi_vignette = load_ndcube_from_fits(wfi_vignetting_model_path)
-nfi_vignette = load_ndcube_from_fits(nfi_vignetting_model_path)
+wfi_vignette = load_ndcube_from_fits(wfi_vignetting_model_path).data[...] + 1E-8
+nfi_vignette = load_ndcube_from_fits(nfi_vignetting_model_path).data[...] + 1E-8
 
 wfi_quartic = create_constant_quartic_coefficients((2048, 2048))
 nfi_quartic = create_constant_quartic_coefficients((2048, 2048))
 
-wfi_quartic[:, :, -2] /= wfi_vignette.data
-nfi_quartic[:, :, -2] /= nfi_vignette.data
+wfi_quartic[:, :, -2] /= wfi_vignette
+nfi_quartic[:, :, -2] /= nfi_vignette
 
 meta = NormalizedMetadata.load_template("FQ1", "1")
 meta['DATE-OBS'] = str(datetime.now())
