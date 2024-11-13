c6a3d9a5d8261b20519c25953c2fe4c4988ba41e
Marcus Hughes <hughes.jmb@gmail.com>
2024-11-12 22:29:15 -0700

    make quartic fit not have large values

--- create_quartic_fit_coeffs.py
+++ create_quartic_fit_coeffs.py
@@ -1,6 +1,7 @@
 # ruff: noqa
 from datetime import datetime
 
+import numpy as np
 from astropy.io.fits import CompImageHDU, HDUList, ImageHDU, PrimaryHDU
 from astropy.wcs import WCS
 from astropy.wcs.docstrings import naxis
@@ -35,14 +36,18 @@ write_ndcube_to_fits(nfi_cube, "nfi_quartic_backward_coeffs.fits")
 wfi_vignetting_model_path = "./build_3_review_files/PUNCH_L1_GM1_20240817174727_v2.fits"
 nfi_vignetting_model_path = "./build_3_review_files/PUNCH_L1_GM4_20240819045110_v1.fits"
 
-wfi_vignette = load_ndcube_from_fits(wfi_vignetting_model_path).data[...] + 1E-8
-nfi_vignette = load_ndcube_from_fits(nfi_vignetting_model_path).data[...] + 1E-8
+wfi_vignette = load_ndcube_from_fits(wfi_vignetting_model_path).data[...]
+nfi_vignette = load_ndcube_from_fits(nfi_vignetting_model_path).data[...]
 
 wfi_quartic = create_constant_quartic_coefficients((2048, 2048))
 nfi_quartic = create_constant_quartic_coefficients((2048, 2048))
 
 wfi_quartic[-2, :, :] = 1/wfi_vignette
 nfi_quartic[-2, :, :] = 1/nfi_vignette
+wfi_quartic[np.isinf(wfi_quartic)] = 0
+wfi_quartic[np.isnan(wfi_quartic)] = 0
+nfi_quartic[np.isinf(nfi_quartic)] = 0
+nfi_quartic[np.isnan(nfi_quartic)] = 0
 
 meta = NormalizedMetadata.load_template("FQ1", "1")
 meta['DATE-OBS'] = str(datetime.now())
