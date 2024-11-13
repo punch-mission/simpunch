dfdbe15dba2c441b03a87ccbc40bffaaa69087f8
s0larish <risht42@gmail.com>
2024-11-13 10:23:23 -0700

    fixing error in add_starfield_polarized module

--- simpunch/tests/test_starfield.py
+++ simpunch/tests/test_starfield.py
@@ -35,7 +35,7 @@ def sample_ndcube() -> NDCube:
 
 @pytest.fixture
 def sample_ndcollection() -> callable:
-    def _create_sample_ndcollection(shape: tuple, code: str = "PM1", level: str = "0") -> NDCollection:
+    def _create_sample_ndcollection(shape: tuple) -> NDCollection:
         wcs = WCS(naxis=2)
         wcs.wcs.ctype = ["HPLN-ARC", "HPLT-ARC"]
         wcs.wcs.cunit = ["deg", "deg"]
@@ -44,16 +44,26 @@ def sample_ndcollection() -> callable:
         wcs.wcs.crval = [1, 1]
         wcs.wcs.cname = ["HPC lon", "HPC lat"]
 
-        meta = NormalizedMetadata.load_template(code, level)
-        meta["DATE-OBS"] = str(datetime(2024, 2, 22, 16, 0, 1))
-        meta["FILEVRSN"] = "1"
+        metaz = NormalizedMetadata.load_template("PZ1", "1")
+        metaz["DATE-OBS"] = str(datetime(2024, 2, 22, 16, 0, 1))
+        metaz["FILEVRSN"] = "1"
 
-        input_data = NDCube(np.random.random(shape).astype(np.float32), wcs=wcs, meta=meta)
+        metam = NormalizedMetadata.load_template("PM1", "1")
+        metam["DATE-OBS"] = str(datetime(2024, 2, 22, 16, 0, 1))
+        metam["FILEVRSN"] = "1"
+
+        metap = NormalizedMetadata.load_template("PP1", "1")
+        metap["DATE-OBS"] = str(datetime(2024, 2, 22, 16, 0, 1))
+        metap["FILEVRSN"] = "1"
+
+        input_data_z = NDCube(np.random.random(shape).astype(np.float32), wcs=wcs, meta=metaz)
+        input_data_m = NDCube(np.random.random(shape).astype(np.float32), wcs=wcs, meta=metam)
+        input_data_p = NDCube(np.random.random(shape).astype(np.float32), wcs=wcs, meta=metap)
 
         return NDCollection(
-            [("-60.0 deg", input_data),
-             ("0.0 deg", input_data),
-             ("60.0 deg", input_data)],
+            [("-60.0 deg", input_data_m),
+             ("0.0 deg", input_data_z),
+             ("60.0 deg", input_data_p)],
             aligned_axes="all",
         )
     return _create_sample_ndcollection
