369c2dfaa4f6d96c9c99b938d7e61849022d3cef
Marcus Hughes <hughes.jmb@gmail.com>
2025-03-02 14:55:42 -0800

    fix cli

--- simpunch/flow.py
+++ simpunch/flow.py
@@ -1,8 +1,10 @@
 """Run the entire pipeline backward."""
 from datetime import datetime, timedelta
+import os
 
 import numpy as np
 from prefect import flow
+from dateutil.parser import parse as parse_datetime_str
 
 from simpunch.level0 import generate_l0_cr, generate_l0_pmzp
 from simpunch.level1 import generate_l1_cr, generate_l1_pmzp
@@ -14,17 +16,18 @@ from simpunch.level3 import generate_l3_ctm, generate_l3_ptm
 def generate_flow(file_tb: str,
                   file_pb: str,
                   out_dir: str,
-                  start_time: datetime,
+                  start_time: datetime | str,
                   backward_psf_model_path: str,
                   wfi_quartic_backward_model_path: str,
                   nfi_quartic_backward_model_path: str,
                   transient_probability: float = 0.03,
                   shift_pointing: bool = False) -> list[str]:
     """Generate all the products in the reverse pipeline."""
-    i = int(file_tb.split("_")[6][4:])
+    i = int(os.path.basename(file_tb).split("_")[4][4:])  # todo: make less specific to the filename
     rotation_indices = np.array([0, 0, 1, 1, 2, 2, 3, 3])
-    rotation_stage = rotation_indices[i % 8]
+    rotation_stage: int = rotation_indices[i % 8]
 
+    start_time = start_time if isinstance(start_time, datetime) else parse_datetime_str(start_time)
     time_obs = start_time + timedelta(minutes=i*4)
     l3_ptm = generate_l3_ptm(file_tb, file_pb, out_dir, time_obs, timedelta(minutes=4), rotation_stage)
     l3_ctm = generate_l3_ctm(file_tb, out_dir, time_obs, timedelta(minutes=4), rotation_stage)
