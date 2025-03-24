c64d43230750b521e253f96cf5e60a604dbf3545
Marcus Hughes <hughes.jmb@gmail.com>
2025-03-23 20:39:34 -0600

    use prefect dask for generation

--- simpunch/flow.py
+++ simpunch/flow.py
@@ -1,10 +1,12 @@
 """Run the entire pipeline backward."""
 import os
-from datetime import datetime, timedelta
+import sys
+from datetime import datetime, timedelta, UTC
 
 import numpy as np
 from dateutil.parser import parse as parse_datetime_str
 from prefect import flow
+from prefect_dask import DaskTaskRunner
 
 from simpunch.level0 import generate_l0_cr, generate_l0_pmzp
 from simpunch.level1 import generate_l1_cr, generate_l1_pmzp
@@ -12,11 +14,11 @@ from simpunch.level2 import generate_l2_ctm, generate_l2_ptm
 from simpunch.level3 import generate_l3_ctm, generate_l3_ptm
 
 
-@flow
+@flow(task_runner=DaskTaskRunner())
 def generate_flow(file_tb: str,
                   file_pb: str,
+                  time_obs: datetime,
                   out_dir: str,
-                  start_time: datetime | str,
                   backward_psf_model_path: str,
                   wfi_quartic_backward_model_path: str,
                   nfi_quartic_backward_model_path: str,
@@ -27,8 +29,6 @@ def generate_flow(file_tb: str,
     rotation_indices = np.array([0, 0, 1, 1, 2, 2, 3, 3])
     rotation_stage: int = rotation_indices[i % 8]
 
-    start_time = start_time if isinstance(start_time, datetime) else parse_datetime_str(start_time)
-    time_obs = start_time + timedelta(minutes=i*4)
     l3_ptm = generate_l3_ptm(file_tb, file_pb, out_dir, time_obs, timedelta(minutes=4), rotation_stage)
     l3_ctm = generate_l3_ctm(file_tb, out_dir, time_obs, timedelta(minutes=4), rotation_stage)
     l2_ptm = generate_l2_ptm(l3_ptm, out_dir)
@@ -37,19 +37,38 @@ def generate_flow(file_tb: str,
     l1_polarized = []
     l1_clear = []
     for spacecraft in ["1", "2", "3", "4"]:
-        l1_polarized.extend(generate_l1_pmzp(l2_ptm, out_dir, rotation_stage, spacecraft))
-        l1_clear.append(generate_l1_cr(l2_ctm, out_dir, rotation_stage, spacecraft))
+        l1_polarized.append(generate_l1_pmzp.submit(l2_ptm, out_dir, rotation_stage, spacecraft))
+        l1_clear.append(generate_l1_cr.submit(l2_ctm, out_dir, rotation_stage, spacecraft))
+    l1_polarized = [entry.result() for entry in l1_polarized]  # this is a list of lists
+    l1_polarized = [item for sublist in l1_polarized for item in sublist]  # so we flatten it
+    l1_clear = [entry.result() for entry in l1_clear]
 
     l0_pmzp = []
     for filename in l1_polarized:
-        l0_pmzp.append(generate_l0_pmzp(filename, out_dir, backward_psf_model_path,  # noqa: PERF401
+        l0_pmzp.append(generate_l0_pmzp.submit(filename, out_dir, backward_psf_model_path,  # noqa: PERF401
                                                wfi_quartic_backward_model_path, nfi_quartic_backward_model_path,
                                                transient_probability, shift_pointing))
+    l0_pmzp = [entry.result() for entry in l0_pmzp]
 
     l0_cr = []
     for filename in l1_clear:
-        l0_cr.append(generate_l0_cr(filename, out_dir, backward_psf_model_path,  # noqa: PERF401
+        l0_cr.append(generate_l0_cr.submit(filename, out_dir, backward_psf_model_path,  # noqa: PERF401
                                                wfi_quartic_backward_model_path, nfi_quartic_backward_model_path,
                                                transient_probability, shift_pointing))
+    l0_cr = [entry.result() for entry in l0_cr]
 
     return [l3_ptm, l3_ctm, l2_ptm, l2_ctm] + l1_polarized + l1_clear + l0_pmzp + l0_cr  # noqa: RUF005
+
+
+if __name__ == "__main__":
+    file_tb = "/Users/mhughes/data/punch_simulation/gamera_jan2024/DOICMEM_00912all_plasma_GLOBAL_step0005_dcmer0000_azequi_TB.fits"
+    file_pb = "/Users/mhughes/data/punch_simulation/gamera_jan2024/DOICMEM_00912all_plasma_GLOBAL_step0005_dcmer0000_azequi_PB.fits"
+    out_dir = "/Users/mhughes/data/punch_simulation/sync_test/"
+    backward_psf_model_path = "/Users/mhughes/data/punch_simulation/inputs/synthetic_backward_psf.fits"
+    wfi_quartic_backward_model_path = "/Users/mhughes/data/punch_simulation/inputs/wfi_quartic_backward_coeffs.fits"
+    nfi_quartic_backward_model_path = "/Users/mhughes/data/punch_simulation/inputs/nfi_quartic_backward_coeffs.fits"
+    transient_probability = 0.0
+    shift_pointing = False
+
+    generate_flow(file_tb, file_pb, datetime.now(UTC), out_dir, backward_psf_model_path,
+                  wfi_quartic_backward_model_path, nfi_quartic_backward_model_path, transient_probability, shift_pointing)
